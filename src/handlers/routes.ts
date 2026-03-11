f04160a96731ebc491f9eb3f45713069738db51c
Remy MACHAVOINE <rmachavoine@scaleway.com>
2026-03-11 12:20:43 +0100

    feat: add products endpoint and docker -compose

--- src/handlers/routes.ts
+++ src/handlers/routes.ts
@@ -1,9 +1,17 @@
-import { Application } from "express";
+import { Application, Request, Response } from "express";
 
 export const initHandlers = (app: Application) => {
     app.get("/", (req, res) => {
         return res.send({
             message: "Hell world"
         })
+    })
+
+    app.get("/products", (req: Request, res: Response) => {
+        return res.send([
+            {id: 1, name:"courgette"},
+            {id: 2, name:"pomme"},
+            {id: 3, name:"ail"}
+        ])
     })
 } 
